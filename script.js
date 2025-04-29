02fa55e4f88f0fb8bcca0fd0b63d301c3b8fd0c6
Kaue1881 <kauerafael18811881@gmail.com>
2025-04-28 21:58:04 -0300

    :tada: Initial Commit done!

---
+++ script.js
@@ -0,0 +1,35 @@
+let ValorSorteado = Math.random() * (100 - 1) + 1; // valor para comparação
+let input = document.querySelector("#palpite"); // unico input da pagina
+let contador = 0; // contador fora da função
+
+function Palpite() {
+    //vai guardar o palpite sem as casas decimais
+    const ValorSorteadoTruncado = Math.trunc(ValorSorteado);
+    // vai guardar o valor do palpite do usuário
+    let ValorSugerido = input.value.trim();
+    //vai contar as tentativas
+
+    //comparação para descobrir o valor
+    if(ValorSugerido == '') {
+        alert("Coloque um valor a caixa vazia");
+        contador++;
+        return;
+    } else if(ValorSorteadoTruncado < ValorSugerido){
+        alert("Mais baixo");
+        contador++;
+    } else if(ValorSorteadoTruncado > ValorSugerido) {
+        alert("Mais alto");
+        contador++;
+    } else {
+        contador++;
+        alert('Parabéns você acertou em ' + contador + ' tentativas');
+    }
+}
+
+function recomecar() {
+    input.innerText = "";
+    ValorSorteado = Math.random() * (100 - 1) + 1;
+    contador = 0;
+
+    alert("Você recomeçou o jogo")
+}
